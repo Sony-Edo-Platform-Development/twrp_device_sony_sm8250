daf931b97b2bba7292279c3fe0fa10de18e917b5
Shion Kagurazaka <tut4n0ta5h10n@tutanota.com>
2022-03-23 19:36:10 +0800

    edo: clean-up the fstab.
    *.Some paths do not exist(like Cache), need to be removed.
    * add slotselect for dynamic partition
    * add advanced backup for some partition (OrangeFox only, when you build
      TWRP, please drop it)
    * enable FBE_V2 reder to https://github.com/hellobbn/android_device_sony_edo-common/commit/b58cacfc7dcf16cd33ee92d9b08f38500943a26e
    * make dynamic partition image flashable

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,6 +106,9 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Partitions (listed in the file) to be wiped under recovery.
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+
 # Workaround for error copying vendor files to recovery ramdisk
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
